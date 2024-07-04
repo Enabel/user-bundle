fdc380ec2a8ca743582a3d61ea1779bf5dd12b0a
Damien Lagae <damienlagae@gmail.com>
2024-07-04 10:56:23 +0200

    ⚡️ Move symfony 7

--- src/DependencyInjection/UserBundleExtension.php
+++ src/DependencyInjection/UserBundleExtension.php
@@ -7,9 +7,9 @@ namespace Enabel\UserBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class UserBundleExtension extends Extension implements PrependExtensionInterface
 {
